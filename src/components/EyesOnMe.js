8a566bc2411ceeb834b9c64a64bad024c23e624c
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-01 10:22:56 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  // Event handler function for when the button receives focus
+  const handleFocus = () => {
+    console.log("Good!");
+  };
+
+  // Event handler function for when the button loses focus
+  const handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Event handler function for input change event
+  const handleChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      {/* Render an input field with type set to "password" and attach onChange event handler */}
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
